87cd57d088f2236dda54a3275d3d62d5eca0ddfb
Hamza Mubeen <eb.blaze47@gmail.com>
2020-01-07 13:44:21 +0500

    used an api to get posts and then render them on the DOM

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+  <h1 v-if = "posts==[]"  > Loading... </h1>
+  <give-post v-else v-for="post in posts" :key="post.id" :title="post.title" :body="post.body">  </give-post>
+</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+
+<script>
+/* eslint-disable */
+import axios from "axios"
+import givePost from "./components/givePost"
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  components:{
+    givePost
+  },
+  created:function(){
+    axios.get("https://jsonplaceholder.typicode.com/posts" ).then(response => { this.posts = response.data;console.log(response.data)  });
+  },
+    data(){
+      return {
+        posts : [],
+      }
+    }
+  
 }
+/* eslint-disable */
 </script>
 
+
+
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/givePost.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container" >
+    <div id="main-heading" >
+      <h1 > {{title}} </h1>
+    </div>
+    <div>
+      <p> {{body}} </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:["title", "body"]
+}
+</script>
+
+<style >
+.container{
+  width:90%;
+  margin-left: 5%;
+  margin-right:5%;
+  margin-top:50px;
+}
+#main-heading{
+  font-size:15px;
+}
+</style>>
